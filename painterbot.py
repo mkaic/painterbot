393f7a503a515ba6a66c8db986b7428d6b6933ec
Kai Christensen <matthew.kai.christensen@gmail.com>
2023-10-09 18:16:31 -0700

    adjust defaults for better timelapses

--- painterbot.py
+++ painterbot.py
@@ -213,7 +213,7 @@ class StrokeGroup(nn.Module):
 
         # Strokes can only ever curve a certain amount to avoid a bunch of
         # obvious perfect circles showing up in the painting
-        self.sigma_theta.data.clamp_(0.001, 0.25)
+        self.sigma_theta.data.clamp_(0.001, 0.15)
 
         self.alpha.data.clamp_(0.01, 1.0)
         self.color.data.clamp_(0.0, 1.0)
@@ -466,7 +466,7 @@ device = "cuda:1"
 
 image_size = 256
 
-target = Image.open("branos.jpg").convert("RGB")
+target = Image.open("source_images/rick.png").convert("RGB")
 
 
 preprocessing = T.Compose(
@@ -483,7 +483,7 @@ target = target
 
 painting, loss_history, mae_history = optimize_grouped_painting(
     target,
-    n_groups=20,
+    n_groups=6,
     n_strokes_per_group=50,
     iterations=300,
     lr=0.01,
@@ -494,212 +494,6 @@ painting, loss_history, mae_history = optimize_grouped_painting(
 
 canvas = torch.zeros(3, 1024, 1024, device=device)
 result = painting.render_timelapse_frames(canvas, "painting_timelapse_frames")
-
-# +
-# EXPERIMENTS LOG
-
-# Baseline: SSIM + PSNR, 300 strokes, 300 iterations, LR=0.05 * 0.99^step.
-# Takes about 5 minutes. reaches MAE=0.045
-
-# Experiment 1: does adding L1 into the mix help?
-# reaches MAE=0.044. not statistically significant.
-
-# Experiment 2: does adding L2 into the mix help?
-# reaches MAE=0.044. not statistically significant.
-
-# Experiment 3: NEW BASELINE, LR=0.01, no decay, 50 strokes for 400 steps.
-# Reaches MAE=0.0738.
-
-# Experiment 4: does decay help? setting lr_gamma to 0.995.
-# Reaches MAE=0.885. decay hurts.
-
-# Experiment 5: higher LR=0.02. try decay again.
-# Reaches MAE = 0.080
-
-# Experiment 6: higher LR=0.02 with no decay.
-# Reaches MAE = 0.072. LR is inconsequential with Adam perhaps?
-
-# Experiment 7: AdamW instead of Adam.
-# Reaches MAE = 0.076. Will stick with the simpler Adam.
-
-# Experiment 8: does iteratively adding strokes once every 1 step help?
-# Reaches MAE = 0.0685. It does!
-
-# Experiment 9: what if we do it every 2 steps?
-# Reaches MAE = 0.0716. Inconclusive, need to compensate for difference in computing power between the approaches.
-
-# Experiment 10: 50 strokes with one introduced every 2 steps means the
-# first 100 steps only take up as much compute as 50 all-strokes steps. This means 50 additional steps can be
-# added to the total to compensate. Thus, the step count is now (total + (stroke_count*ramp_stride/2)).
-# In this case, that's 450 steps.
-# Reaches MAE = 0.0706. Inconclusive.
-
-# Experiment 11: let's ramp up to ramp_stride = 8 for the heck of it.
-# Reaches MAE = 0.0685. I think this strategy can work, just need to once again reset the baseline.
-
-# Experiment 12: 50 strokes, 1000 iterations. No ramping. Let's reset the baseline.
-# Reaches MAE = 0.0637
-
-# Experiment 13: same as new baseline, but with ramping and ramp_stride set to 20. This means
-# all strokes won't be active until step 1000, and to keep compute parity we'll need to set
-# total steps to 1500.
-# Reaches MAE = 0.0636. seems to make no real difference, but I like it so let's keep it.
-
-# Experiment 14: scaling up the baseline to 300 strokes, 1000 steps.
-# Reaches MAE = 0.0354 and Loss = -1.751
-
-# Experiment 15: 300 strokes, ramp_stride=3, 1450 steps (to compensate for compute disparity)
-# Reaches MAE = 0.03651 and Loss = -1.762. No difference. is learning rate to blame?
-
-# Experiment 16: same as #14, but lower the learning rate to 0.003.
-# Reaches MAE = 0.0413 and Loss = -1.675. Lower learning rate is bad.
-
-# Experiment 17: totally reworked the system. Now the painting optimizes one group of strokes at a time, then
-# freezes them. When the number of strokes in a group is 1, this is equivalent to my original approach, just
-# using SSIM and PSNR as metrics. So, for this run, to establish a baseline,
-# I am setting strokes and groups to 300. Each group is optimized for 100 steps.
-# Reaches MAE = 0.0875 and Loss = -1.457. This approach still sucks!
-
-# Experiment 18: trying a group size of 10 strokes per group, so 30 groups. Since there are 10x
-# fewer groups, I optimize each for 10x as long, 1000 steps. Same computational load as #17, but
-# hopefully far superior quality.
-# Reaches MAE = 0.050 and Loss = -1.76. This approaches the quality of optimizing all 300 in parallel!
-# This doesn't really show definite *improvement* yet though since we still used the same compute
-# budget as the parallel optimization.
-
-# Experiment 19: Let's set another baseline. 300 strokes, 5 groups, 500 iterations per group. Much
-# lower budget than previous experiment, but bigger group size.
-# Reached MAE = 0.0579 and Loss = -1.67. hmmm.
-
-# Experiment 20: It would seem that 1000 iterations is pretty necessary for convergence. Let's go
-# with 300 strokes, 6 groups of 50 each, and 1000 iterations per group.
-# Reached MAE = 0.045 and Loss = -1.80. Pretty good, but less efficient than just optimizing in
-# one big group.
-
-# Experiment 21: 300 strokes, 30 groups of 10 each, 300 iterations per group.
-# Reached MAE = 0.0608 and Loss = -1.666. ugh.
-
-# Experiment 22: Let's make things faster. Reduce steps by a factor of 3 (1000 to 300)
-# Raise the learning rate a factor of 3 (0.01 to 0.03)
-# Reached MAE = 0.0581 and Loss = -1.653.  Not great, I think learning rate decay may actually be useful here?
-
-# Experiment 23: Cosine decay on the big 0.03 LR? Otherwise same as #22.
-# Reached MAE = 0.0598 and Loss = -1.646. No change.
-
-# Experiment 24: Multiplicative (gamma = 0.99) decay on the big 0.03 LR. Otherwise same as #22.
-# Reached MAE = 0.0624 and Loss = -1.633. No change.
-
-# Experiment 25: Okay, once and for all, does learning rate or LR decay matter?
-# Same as #22, but with LR=0.01. So LR=0.01, strokes = 300, steps = 300.
-# MAE: 0.05807 | Loss: -1.67026. Meh. I'm not messing with decay any more.
-
-# Experiment 26: what if I ditch PSNR and only use SSIM?
-# Loss: -0.66356 | MAE: 0.09006. meeeehhhhhh weird bad color accuracy.
-
-# Experiment 27: what if i go back to the old loss but divide PSNR by 30 instead of 20.
-# MAE: 0.06551 | Loss: -1.28067
-
-# Experiment 28: What if instead of reducing PSNR's influence compared to 22, I double it?
-# MAE = 0.05987 | Loss = -2.67799. Insigificant difference from 22.
-
-# Experiment 29: 500 strokes, 50 groups, 300 iterations.
-# MAE = 0.05246 | Loss = -1.75182
-
-# Experiment 30: 300 strokes, 6 groups, 300 iterations. LR = 0.01. New baseline.
-# MAE = 0.05699 | Loss = -1.68893 
-
-# Experiment 31: 300 strokes, 6 groups, 300 iterations. Trying out a new loss where I compare against the
-# change each stroke *should* have made instead of just directly comparing against the image.
-# MAE = 0.06615 | Loss = -1.65992. No real change.
-
-# Experiment 32: Same as #30, but with the old loss thrown in too.
-# MAE = 0.05845 | Loss = -1.63180 Nope, just get rid of the new loss. Doesn't do anything.
-
-# Experiment 33: same as #30, but with multi-scale SSIM instead of regular SSIM
-# MAE = 0.05276 | Loss = -1.86894. Ayyy finally an improvement!
-
-# Experiment 34: same as #33, but tacking on a Sobel edge loss to see if that does anything.
-# MAE = 0.06488 | Loss = -1.45236. Made it considerably worse.
-
-# Experiment 35: same as 34, but divide sobel maps by 8 before 
-# comparing as suggested by ptrblck on the PyTorch forums.
-# MAE = 0.05722 | Loss = -1.76451. Doesn't help. Sobel loss bad.
-
-# Experiment 35: 1000 strokes, 10 groups, 500 iterations, LR=0.01
-# MAE = 0.03450 | Loss = -2.13276
-
-# Experiment 36: Better initialization. I'm setting the color based on the actual color of
-# the target image, the position is sampled based on where the reconstruction error is highest,
-# and the stroke size is made proportional to the MAE. All these things can still be optimized
-# if algorithm wants to, but they should be starting out way less random now.
-# 300 strokes, 6 groups, 300 iterations. Also, it would appear that by setting theta to always be 1,
-# I removed the optimizer's ability to rotate the strokes. whoops.
-# MAE = 0.03173 | Loss = -2.14367. Output is blocky but accurate.
-
-# Experiment 37: same as 36, but went back to randomly initializing the rotation of the gaussians.
-# I want a smoother output this time, as the blockiness looks jarring. For some reason, the rotation
-# of the gaussians is not being optimized?
-# forgot what the numbers were here but it was marginally better than 36.
-
-# Experiment 38: fixed theta optimization. Apparently it NEVER WORKED??? LIKE AT ALL???
-# Now that theta can ACTUALLY be optimized I'm really really excited about the potential for
-# improvement. Fingers crossed!
-# MAE = 0.02872 | Loss = -2.21096 OMG OMG OMG OMG OMG OMG
-
-# Experiment 39: 300 strokes, 300 groups, 100 iterations.
-# MAE = 0.05021 | Loss = -1.90338. Bad!
-
-# Experiment 40: 500 strokes, 10 groups, 300 iterations.
-# MAE = 0.02323 | Loss = -2.32215 WOOOOOOOOO
-
-# SWITCHING TO THE MONA LISA FOR TESTING
-
-# Mona Lisa 1: 500 strokes, 10 groups, 100 iterations. LR=0.01.
-# MAE = 0.03102 | Loss = -2.27161
-
-# Mona Lisa 2: 500 strokes, 10 groups, 100 iterations. LR=0.03 BIG LR
-# MAE = 0.03430 | Loss = -2.19209
-
-# Mona Lisa 3: 500 strokes, 10 groups, 100 iterations. LR=0.01. 
-# lowered adam betas to (0.8, 0.9) bc i'm only optimizing for 100 iterations.
-# MAE = 0.030
-
-# Mona Lisa 4: same as 3, but with even lower betas=(0.66, 0.66)
-# MAE = 0.031
-
-# Mona Lisa 5: Same as 4, but with betas=(0.8,0.9) and MAE directly included in the loss
-# MAE = 0.03035 | Loss = -2.25097. Removed MAE after this
-
-# Mona Lisa 6: same as 2, but with 20 groups instead of 10. and the newest betas as well.
-# LR = 0.03, because I'm not ready to give up on a big learning rate yet. 100 iterations
-# should be plenty for this, but I gotta figure out the hyperparams to make that work!
-# MAE = 0.035, not great. I think big learning rate is just not gonna happen.
-
-# Mona Lisa 7: back to LR=0.01, but still 100 iterations and betas=(0.8,0.9)
-# Same setup as 3, but this time I'm setting the softmax temperature to 0.5
-# MAE = 0.03172 | Loss = -2.24758
-
-# Mona Lisa 8: Same as 7, but now softmax temp is 0.25.
-# MAE = 0.03138 | Loss = -2.25816 basically exactly the same. no change.
-
-# Mona Lisa 9: keepign low softmax temp and bumping groups from 10 to 50. I have a theory
-# that more frequent sampling of the error map combined with more agressive error-targeting
-# could result in better reconstruction.
-# MAE = 0.03196 | Loss = -2.25030. No difference. seems like the temperature and group numbers
-# simply don't matter that much.
-
-# NEW BASELINE IN PREPARATION FOR CURVED STROKE TESTING
-# Mona Lisa, 100 strokes, 200 iterations, 3 groups.
-# MAE = 0.04143 | Loss = -2.02877
-
-# WITH POLAR COORDINATE WARPED GAUSSIANS????
-# Mona Lisa, 100 strokes, 200 iterations, 3 groups, POLAR WARPING ENABLED
-# MAE = 0.04146 | Loss = -2.00878. Nearly identical loss but i don't care, i worked too hard on
-# getting the polar warping to work.
-
-# Polar coords activated, 500 strokes, 10 groups, 300 iterations.
-# shooting for a new lowest loss on Mona Lisa.
-# 
 
 # +
 loss_history_arr, mae_history_arr = np.array(loss_history), np.array(mae_history)
