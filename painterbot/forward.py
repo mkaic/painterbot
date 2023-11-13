fix non-square aspect ratio bug and begin preliminary investigation of resolution scaling.

painterbot/forward.py
@@ -1,7 +1,6 @@
 import shutil
 from pathlib import Path
 
-import kornia
 import torch
 from torchvision.transforms.functional import to_pil_image
 
@@ -70,7 +69,6 @@ def rgb_to_oklab(rgb: torch.Tensor) -> torch.Tensor:
 # print(colour.convert([1.0, 1.0, 0.0], "sRGB", "Oklab"))
 
 
-@torch.compile
 def loss_fn(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
     # convert to perceptually uniform color space
     x = rgb_to_oklab(x)
@@ -82,12 +80,10 @@ def loss_fn(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
         torch.sum(torch.square(x[:, 1:, :, :] - y[:, 1:, :, :]), dim=1)
     )
 
-    edges_x = kornia.filters.sobel(x)
-    edges_y = kornia.filters.sobel(y)
+    return luminance_l2 + chroma_squared_euclidean_distance
 
-    edges_l2 = torch.mean(torch.square(edges_x - edges_y))
 
-    return luminance_l2 + chroma_squared_euclidean_distance + edges_l2
+loss_fn_compiled = torch.compile(loss_fn)
 
 
 def forward(
@@ -107,7 +103,7 @@ def forward(
         target = target.unsqueeze(0).expand(
             parameters.n_strokes, -1, -1, -1
         )  # (3 x H x W) -> (N x 3 x H x W)
-        loss = loss_fn(canvas_history, target)
+        loss = loss_fn_compiled(canvas_history, target)
 
         return canvas.detach(), loss
 

painterbot/optimize.py
@@ -3,7 +3,7 @@ import shutil
 from pathlib import Path
 
 import torch
-from torchvision.transforms.functional import to_pil_image
+from torchvision.transforms.functional import to_pil_image, resize
 from tqdm.auto import tqdm
 
 from .forward import forward
@@ -24,23 +24,23 @@ def optimize(
         show_inner_pbar = True
 
     device = target.device
-    width, height = target.shape[-2:]
+    height, width = target.shape[-2:]
     width_to_height_ratio = width / height
 
-    canvas = torch.zeros_like(target)
-
-    frozen_params: StrokeParameters = None
+    frozen_params = StrokeParameters(
+        n_strokes=0,
+        width_to_height_ratio=width_to_height_ratio,
+    ).to(device)
 
     output_path = Path("timelapse_frames_optimization")
     if output_path.exists():
         shutil.rmtree(output_path)
     output_path.mkdir()
 
-    loss_history = []
-    mae_history = []
-
     outer_pbar = tqdm(range(n_groups))
-    outer_pbar.set_description(f"Loss=?, MAE=? | Group 1/{n_groups}")
+    outer_pbar.set_description(f"Loss=?, MAE=? | Group 1/{n_groups} | Resolution=?")
+
+    canvas = torch.zeros_like(target)
 
     for i in outer_pbar:
         active_params = StrokeParameters(
@@ -53,9 +53,18 @@ def optimize(
             canvas=canvas,
         )
 
+        resolution = (
+            256  # max(min(min(height, width), frozen_params.n_strokes.item()), 64)
+        )
+        target_resized = resize(target, resolution, antialias=True)
+        canvas_resized = resize(canvas, resolution, antialias=True)
+
         optimizer = torch.optim.Adam(
             active_params.parameters(), lr=lr, betas=(0.8, 0.9)
         )
+
+        best_loss = 100.0
+        steps_since_best = 0
 
         if show_inner_pbar:
             inner_iterator = tqdm(range(iterations), leave=False)
@@ -63,10 +72,13 @@ def optimize(
             inner_iterator = range(iterations)
 
         for j in inner_iterator:
+            if steps_since_best > 32:
+                break
+
             result, loss = forward(
-                canvas=canvas,
+                canvas=canvas_resized,
                 parameters=active_params,
-                target=target,
+                target=target_resized,
             )
 
             loss.backward()
@@ -77,31 +89,38 @@ def optimize(
 
             active_params.clamp_parameters()
 
-            mae = torch.mean(torch.abs(result - target))
+            mae = torch.mean(torch.abs(result - target_resized))
 
             if show_inner_pbar:
-                description = f"Loss={loss:.5f}, MAE={mae:.5f}"
+                description = f"Loss={loss:.6f}, MAE={mae:.5f}"
                 inner_iterator.set_description(description)
 
             if log_every is not None and j % log_every == 0:
-                to_pil_image(result).save(output_path / f"{i:05}_{j:06}.jpg")
+                to_pil_image(resize(result, min(height, width), antialias=True)).save(
+                    output_path / f"{i:05}_{j:06}.jpg"
+                )
 
-            loss_history.append(loss.detach().cpu().item())
-            mae_history.append(mae.detach().cpu().item())
+            if loss < best_loss:
+                best_loss = loss
+                steps_since_best = 0
+            else:
+                steps_since_best += 1
 
-        canvas = forward(
-            canvas=canvas,
-            parameters=active_params,
-        )
         if i == 0:
             frozen_params = active_params
         else:
             frozen_params = concat_stroke_parameters([frozen_params, active_params])
 
-        outer_pbar.set_description(
-            f"Loss={loss:.5f}, MAE={mae:.5f} | Group {i+1}/{n_groups}"
+        canvas = torch.zeros_like(target)
+        canvas = forward(
+            canvas=canvas,
+            parameters=frozen_params,
         )
-    return frozen_params, loss_history, mae_history
+
+        outer_pbar.set_description(
+            f"Loss={loss:.5f}, MAE={mae:.5f} | Group {i+1}/{n_groups} | Resolution={resolution}"
+        )
+    return frozen_params
 
 
 if __name__ == "__main__":
@@ -116,11 +135,13 @@ if __name__ == "__main__":
         dtype=torch.float32,
     )
 
-    n_groups = 16
+    n_groups = 64
     n_strokes_per_group = 64
-    iterations = 300
+    iterations = 256
 
-    parameters, loss_history, mae_history = optimize(
+    print("Total strokes:", n_groups * n_strokes_per_group)
+
+    parameters = optimize(
         target,
         n_groups=n_groups,
         n_strokes_per_group=n_strokes_per_group,